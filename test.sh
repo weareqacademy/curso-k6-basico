5d7bde33dd25d4bb6b8c135724bf0b07092cdcdf
Luis.Rossi <luishsr@unesc.net>
2023-07-07 11:25:29 -0300

    teste

---
+++ test.sh
@@ -0,0 +1,4 @@
+curl --request POST \
+     --url http://localhost:3333/signup \
+     --header 'Content-Type: application/json' \
+     --data '{"email": "papito@qaademy.io", "password": "pwd123" }'
